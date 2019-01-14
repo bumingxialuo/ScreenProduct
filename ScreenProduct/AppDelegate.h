e2ec95e36ec85264b4022b046bb3b4fa11dcff25
bumingxialuo <690148814@qq.com>
2019-01-14 13:54:17 +0800

    Initial Commit

---
+++ ScreenProduct/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScreenProduct
+//
+//  Created by 不明下落 on 2019/1/14.
+//  Copyright © 2019 不明下落. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScreenProduct/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ScreenProduct
+//
+//  Created by 不明下落 on 2019/1/14.
+//  Copyright © 2019 不明下落. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
